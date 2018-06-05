4d52e4c81c149e665cab1ff1500ab000a00a2274
Carol <caroline.bastos03@gmail.com>
2018-06-05 14:40:46 +0100

    agora vai

---
+++ src/org/academiadecodigo/bootcamp/CollisionDetector.java
@@ -0,0 +1,60 @@
+package org.academiadecodigo.bootcamp;
+
+/**
+ * Created by codecadet on 04/06/2018.
+ */
+public class CollisionDetector {
+
+    private Enemies[] enemies;
+    private Player player;
+    private double xmediumPlayer;
+    private double ymediumPlayer;
+    private double radiusPlayer;
+
+    private double xmediumEnemy;
+    private double ymediumEnemy;
+    private double radiusEnemy;
+
+    private double centersDistance;
+
+
+
+
+    public boolean check() {
+
+        xmediumPlayer = player.getPicture().getX() + (player.getPicture().getWidth() / 2);
+        ymediumPlayer = player.getPicture().getY() + (player.getPicture().getHeight() / 2);
+        radiusPlayer = player.getPicture().getWidth() / 2;
+
+        for (Enemies enemy : enemies) {
+
+            xmediumEnemy = enemy.getPicture().getX() + (enemy.getPicture().getWidth() / 2);
+            ymediumEnemy = enemy.getPicture().getY() +(enemy.getPicture().getHeight() / 2);
+            radiusEnemy = enemy.getPicture().getWidth() / 2;
+
+            centersDistance = centerDistance(enemy);
+
+            if(centersDistance < (radiusPlayer + radiusEnemy)){
+                return true;
+            }
+
+        }
+
+        return false;
+
+    }
+
+    public double centerDistance(Enemies enemy) {
+
+        double cateto1 = xmediumPlayer - xmediumEnemy;
+
+        double cateto2 = ymediumPlayer - ymediumEnemy;
+
+        double hipotenusa = Math.sqrt(Math.pow(cateto1,2) + Math.pow(cateto2,2));
+
+        return hipotenusa;
+
+    }
+
+
+}
---
+++ src/org/academiadecodigo/bootcamp/Grid.java
@@ -0,0 +1,56 @@
+package org.academiadecodigo.bootcamp;
+
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+/**
+ * Created by codecadet on 04/06/2018.
+ */
+public class Grid {
+
+    public final int PADDING = 10;
+
+    private Rectangle canvas;
+
+    private Rectangle frameRight;
+
+    private Rectangle frameLeft;
+
+
+
+
+    public Grid(int width, int height, int widthFrame) {
+        this.canvas = new Rectangle(PADDING,PADDING,width,height);
+        this.frameRight = new Rectangle(width - widthFrame + PADDING, PADDING, widthFrame, height);
+        this.frameLeft = new Rectangle(PADDING, PADDING, widthFrame, height);
+
+        Picture lisboa = new Picture(PADDING,PADDING,"resources/lisboa.jpg");
+        lisboa.draw();
+
+        Picture oceano = new Picture(PADDING,PADDING,"resources/ocean.png");
+        oceano.draw();
+
+
+    }
+
+
+    public void init(){
+        canvas.draw();
+        frameLeft.fill();
+        frameRight.fill();
+
+    }
+
+
+    public Rectangle getCanvas() {
+        return canvas;
+    }
+
+    public Rectangle getFrameRight() {
+        return frameRight;
+    }
+
+    public Rectangle getFrameLeft() {
+        return frameLeft;
+    }
+}
